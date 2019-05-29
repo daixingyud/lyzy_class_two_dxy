0b4c2a4db2f16c763eafe6bd4add6dd6eee6af6e
1351224063@qq.com <1351224063@qq.com>
2019-05-29 22:21:09 +0800

    小黄书

---
+++ Xiaohuangshu/app/src/main/java/com/example/asus/xiaohuangshu/B.java
@@ -0,0 +1,13 @@
+package com.example.asus.xiaohuangshu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class B extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.b);
+    }
+}
